0bc7199b5ac14b448ffde3b35af1845b8fd30e7e
Konstantin Popov <popov.k.g@gmail.com>
2022-09-11 16:00:32 +0300

    done steps 2-3

---
+++ contact.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const fs = require("fs/promises");
+const { v4: uuid } = require("uuid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const result = await fs.readFile(contactsPath);
+
+    return JSON.parse(result);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find(({ id }) => id === contactId);
+
+    if (!contact) {
+      throw new Error("Contact not found");
+    }
+
+    console.log(contact);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+
+    if (contactIndex === -1) {
+      throw new Error("Contact not found");
+    }
+
+    contacts.splice(contactIndex, 1);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf8");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: uuid(), name, email, phone };
+
+    contacts.push(newContact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf8");
+  } catch (err) {
+    console.log(err);
+  }
+}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,11 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contact.js");
+
+// listContacts();
+// getContactById("9a026613-a24b-4100-b928-6a7f4c6320d3");
+// removeContact("9a026613-a24b-4100-b928-6a7f4c6320d3");
+// addContact("D.A.Ta", "data@mail.com", "(111) 222-33-44");
